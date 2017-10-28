c6c149840ddf6fe0f23b492da7aa3c49b16c94e0
TaylorHan <rhaktkdl@gmail.com>
2017-10-28 11:54:25 +0900

    test

---
+++ PickMe/Assets/Scripts/SetResolution.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Screen.SetResolution(1280, 720, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
