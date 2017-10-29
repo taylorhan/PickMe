ecc94609a5398695608c35798a82b89bbc3d0aa5
jinhueck <qorgh9184@gmail.com>
2017-10-29 09:48:32 +0900

    SoundManager

---
+++ PickMe/Assets/Script/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioSource pickup;
+
+	public void PickupPlay(){
+		pickup.Play ();
+	}
+}
